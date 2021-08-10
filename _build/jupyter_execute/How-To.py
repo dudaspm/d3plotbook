73b5d2849daa97b0a4ff56101a5f5afe2f2f0caa
Patrick M. Dudas <dudaspm@gmail.com>
2021-08-10 16:38:20 -0400

    adding three.js + Aero

---
+++ _build/jupyter_execute/How-To.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook Introduction
+
+# I know it tradition to have the refences at the end of books, but when you are standing on the shoulders of giants. You thank them first. 
+
+# ## References
+
+# ```{bibliography}
+# ```
+
+# In[ ]:
+
+
+
+
