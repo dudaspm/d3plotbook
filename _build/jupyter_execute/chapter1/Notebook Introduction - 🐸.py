4cdcf38525de629c08d3b6798e33f72a08e7cc3b
Patrick Dudas <dudaspm@gmail.com>
2022-01-10 16:57:25 -0500

    updating the "Troubleshooting" section

---
+++ _build/jupyter_execute/chapter1/Notebook Introduction - 🐸.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook Introduction - 🐸
+
+# This notebook allows you to both follow the text and interact with the code directly. 
+# 
+# At the top page, you will see:
+# 
+# <img src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen1.png" alt="icons at the top right corner of the screen" style="width:200px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# Select the icon on the left, the rocket ship:
+# 
+# <img src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen2.png" alt="rocket ship icon for running the notebook" style="width:200px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# Then go down to "Live Code":
+# 
+# 
+# <img src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen4.png" alt="selecting Live Code" style="width:240px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# You should see at the top of the page a loading bar that cycles through mulitple states. 
+# <img src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen6.png" alt="loading bar, first cycle" style="width:240px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# <img src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen7.png" alt="loading bar, second cycle" style="width:240px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# <img src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen7b.png" alt="loading bar, thrid cycle" style="width:240px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# <img alt="loading bar, fourth cycle" src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen8.png" style="width:240px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# Then, finally:
+# <img src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen9.png" alt="loading bar, final cycle" style="width:240px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# Try this for yourself! 
+# 
+# Here is a basic line in Python, after setting to Live Code. You can edit the Python code **directly in the notebook** 😀 😀 😀
+
+# In[1]:
+
+
+2 + 2
+
+
+# ## Export to Binder or Google Colab 
+
+# As you can see you can also export these notebooks to Binder or Google Colab. 
+# 
+# <img alt="Binder or Google Colab options" src="https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen3.png" style="width:240px;margin-left:auto;margin-right:auto;display:block;">
+# 
+# This will take you to their respective websites but you can work with them there, if you would like!
+
+# I know it tradition to have the refences at the end of books, but when you are standing on the shoulders of giants. You thank them first. 
+
+# ## References
+
+# ```{bibliography}
+# ```
+
+# In[ ]:
+
+
+
+
--- _build/jupyter_execute/chapter1/Troubleshooting D3.js Issues.py
+++ _build/jupyter_execute/chapter1/Troubleshooting D3.js Issues.py
@@ -5,19 +5,17 @@
 
 # ## Preface
 # 
-# I have run into 1000's of issues during my development time with D3.js. This will be a growing document to express the issues that were more common and basics on debugging. 
+# I have run into 1000s of issues during my development time with D3.js. This page will be a growing document to express the more common problems and basics of debugging. 
 
 # ## Debugging
 # 
-# I will provide a perspective from Chrome, but these ideas will carry through other browsers as well. 
+# I will provide a perspective from Chrome, but these ideas will also carry through other browsers. 
 
 # ### What is a debugger? 
 # 
-# There are much better places to learn more about debuggers, but at a high-level you use them to debug your code. Too be honest, it is mostly used to see errors or warning (usually, errors). Especially, when you are expecting to see something and that something does not show up. 
+# There are much better places to learn more about debuggers, but you use them to debug your code at a high level. To be honest, it is mostly used to see errors or warnings (usually, errors). Especially when you expect to see something and that something does not show up. 
 
-# #### Accessing the Debugger (Developer Tools)
-# 
-# When you have Chrome (or another browser open) and you create your code and...! Nothing happens. Your first stop should be, to find the Developer Tools. 
+# When you have Chrome (or another browser open), and you create your code and...! Nothing happens. Your first stop should be to find the Developer Tools. 
 # 
 # ```{image} https://raw.githubusercontent.com/dudaspm/ProjectiOn/master/D3Tutorial/Images/debugger1.png
 # :alt: Finding the Developer Tools
@@ -45,3 +43,79 @@
 # :align: center
 # ```
 # <br>
+
+# There is a lot to unpack in this screen, but we will focus on the Console for our conversation. I would highly recommend learning more about what is available in the Developer Tools, but this is a bit too much for our discussion today. Please, see [Chrome Developer Tools](https://developer.chrome.com/docs/devtools/javascript/) for more information. 
+
+# For now, we will stick with only the debugger located in the Console (highlighted below).
+# 
+# ```{image} https://raw.githubusercontent.com/dudaspm/ProjectiOn/master/D3Tutorial/Images/debugger3.png
+# :alt: Finding the Developer Tools (Console highlighted)
+# :class: debugging
+# :width: 800px
+# :align: center
+# ```
+# <br>
+
+# In the console, we will see errors pop up when running code. Let's see an example.
+# 
+# #### Example Error and Developer Tool 
+
+# In[1]:
+
+
+get_ipython().run_cell_magic('html', '', '<script type="module">\n    \n    someVariable + 2 \n    \n</script>')
+
+
+# &#8593; Code will not run &#8593;
+
+# Let's take a look at the Console.
+# 
+# ```{image} https://raw.githubusercontent.com/dudaspm/ProjectiOn/master/D3Tutorial/Images/debugger4.png
+# :alt: Finding the Developer Tools (Console highlighted)
+# :class: debugging
+# :width: 800px
+# :align: center
+# ```
+# <br>
+# 
+# The error will specify two critical things:
+# * What the error is (good for Google Searching)
+#     * In this case, "Uncaught ReferenceError: someVariable is not defined."
+# * Where the error occurred
+#     * In this case, line 3. 
+#     
+# Now, you may ask yourself, isn't 
+# 
+# ```javascript
+# someVariable + 2 
+# ``` 
+# 
+# on line 3? You would be correct, but our first line tells Jupyter to run HTML code. So, start counting after
+# ```python
+# %%html
+
+# ### Console Logs
+# 
+# The next big thing related to the Console or the Debugging Tools, is the ability 
+
+# ## Your Turn
+
+# Create a new cell and run the following code. Then use the Console to see the error and resolve it. 
+# 
+# ```python
+# %%html
+# <script type="module">
+#     
+#     var someVariable = 2 
+#     someVariable = someVariable * 2
+#     someVariable = somevariable - 1
+#     console.log(someVariable)
+#     
+# </script>
+# ```
+
+# In[ ]:
+
+
+
+
